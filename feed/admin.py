db98d11b85d0209f6cd30c750ac9e4be05adf7d3
Abner Jade Alexson <146349299+AbnerAlexson@users.noreply.github.com>
2024-12-02 15:43:48 +0800

    register the Post model

--- feed/admin.py
+++ feed/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Post
 
 # Register your models here.
+
+class PostAdmin(admin.ModelAdmin): # register model into admin
+    pass
+
+admin.site.register(Post, PostAdmin) # connects Post model with PostAdmin
