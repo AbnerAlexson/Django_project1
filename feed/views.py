46deafe30d7c4d60efca8ff07f38bb97ba0ad0e3
Abner Jade Alexson <146349299+AbnerAlexson@users.noreply.github.com>
2024-12-02 16:51:34 +0800

    change template directory from til to feed

--- feed/views.py
+++ feed/views.py
@@ -4,7 +4,7 @@ from .models import Post
 
 class HomePage(ListView): # this class is to render a template with all the post 
     http_method_names = ["get"] # users on the homepage can only GET posts and not modify them
-    template_name = 'homepage.html'
+    template_name = 'feed/homepage.html'
     model = Post
     context_object_name = "posts"
     queryset = Post.objects.all().order_by('-id')[0:30] # homepage shows earliest 40 posts
