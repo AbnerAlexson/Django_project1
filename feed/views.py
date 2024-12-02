61a76f2c86ea409816a240fbf38fa55bf530ca45
Abner Jade Alexson <146349299+AbnerAlexson@users.noreply.github.com>
2024-12-02 16:17:36 +0800

    added Homepage class based views

--- feed/views.py
+++ feed/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from .models import Post
+
+class HomePage(ListView): # this class is to render a template with all the post 
+    http_method_names = ["get"] # users on the homepage can only GET posts and not modify them
+    template_name = 'homepage.html'
+    model = Post
+    context_object_name = "posts"
+    queryset = Post.objects.all().order_by('-id')[0:30] # homepage shows earliest 40 posts
