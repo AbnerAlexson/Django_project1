fecf218a5dc00344b9c7756d6fdcfa33f667ca28
Abner Jade Alexson <146349299+AbnerAlexson@users.noreply.github.com>
2024-12-05 15:48:24 +0800

    added authors for post

--- feed/models.py
+++ feed/models.py
@@ -1,8 +1,13 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 class Post(models.Model):
     text = models.CharField(max_length=240)
     date = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey( # this is the author of the post 
+        User,
+        on_delete=models.CASCADE, # deletes author when post is the delete
+    )
 
 
     def __str__(self):
