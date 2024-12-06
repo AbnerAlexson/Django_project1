01cece81cdf6cda0101a9b750a43e287e80c0ab2
Abner Jade Alexson <146349299+AbnerAlexson@users.noreply.github.com>
2024-12-06 13:33:48 +0800

    added url for create post

--- feed/urls.py
+++ feed/urls.py
@@ -6,5 +6,6 @@ app_name = 'feed'
 
 urlpatterns = [
     path('', views.HomePage.as_view(), name='index'),
-    path("<int:pk>/", views.PostDetailView.as_view(), name='detail') # <int:pk> means the domain name and then /
+    path("<int:pk>/", views.PostDetailView.as_view(), name='detail'), # <int:pk> means the domain name and then /
+    path("new/", views.CreateNewPost.as_view(), name="new_post"),
 ]
