a15eb10b0a1c758b15dc68a5d0cb48c07233ed35
Abner Jade Alexson <146349299+AbnerAlexson@users.noreply.github.com>
2024-12-09 17:13:40 +0800

    create view for profiles

--- profiles/views.py
+++ profiles/views.py
@@ -1,5 +1,10 @@
 from django.contrib.auth.models import User
 from django.views.generic import DetailView
 
-
-
+class ProfileDetailView(DetailView):
+    http_method_names = ["get"]
+    template_name = 'profiles/detail.html'
+    model = User
+    context_object_name = "user"
+    slug_field = "username" #context on how to get profile
+    slug_url_kwarg = "username"
